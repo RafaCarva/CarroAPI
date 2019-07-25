84dad4e53dfa10f3beed9dd79341ecf18f51b607
RafaCarva <rafael.faria.carvalho@gmail.com>
2019-07-24 22:56:03 -0300

    Repository refactor

---
+++ src/CarroAPI.Repository/Repository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Dapper;
+using Microsoft.Extensions.Logging;
+using CarroAPI.Repository;
+using CarroAPI.Domain;
+
+namespace WebStoreAPI.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : CarroAPI.Domain.IEntity
+    {
+        private readonly ILogger<Repository<T>> _logger;
+        public Repository(ILogger<Repository<T>> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Add(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Carro FindByKey(Guid key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Query(string query)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                conn = CarroAPI.Repository.Utilities.ConnectionFactory.GetConnection();
+
+                var result = conn.Query<T>(query);
+
+                return result.ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                throw;
+            }
+            finally
+            {
+                if ((conn != null) && (conn.State != ConnectionState.Closed))
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ src/CarroAPI.Repository/Utilities/ConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CarroAPI.Repository.Utilities
+{
+    public static class ConnectionFactory
+    {
+        private static string _sqlConnection =
+    "server=23.98.153.101;database=DeveloperDB09;user=developer;password=dev123DEV123";
+        public static SqlConnection GetConnection()
+        {
+            var conn = new SqlConnection(_sqlConnection);
+
+            conn.Open();
+
+            return conn;
+        }
+    }
+}
